f1fc6ea7b2690735b983a5a2560ca472506c917c
BrianDX <boris.zaitsev@devexpress.com>
2019-09-17 16:17:16 +0300

    update 18.2

--- CS/Dashboard_CreateScatterChart/Form1.cs
+++ CS/Dashboard_CreateScatterChart/Form1.cs
@@ -8,16 +8,15 @@ namespace Dashboard_CreateScatterChart {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            // Creates a new dashboard and data source for this dashboard.
             Dashboard dashboard = new Dashboard();
 
-            // Creates an OLAP dashboard data source based on the connection string 
+            // Create an OLAP dashboard data source with the connection string 
             // stored in the app.config file.
             DashboardOlapDataSource olapDataSource = new DashboardOlapDataSource();
             olapDataSource.ConnectionName = "AdventureWorksString";
             dashboard.DataSources.Add(olapDataSource);
 
-            // Creates a scatter chart and binds it to data.
+            // Create a scatter chart and binds it to data.
             ScatterChartDashboardItem scatterChart = new ScatterChartDashboardItem();
             scatterChart.DataSource = olapDataSource;
             scatterChart.AxisXMeasure = new Measure("[Measures].[Sales Amount]");
@@ -25,8 +24,8 @@ namespace Dashboard_CreateScatterChart {
             scatterChart.Arguments.Add(new Dimension("[Product].[Category].[Category]"));
             scatterChart.Weight = new Measure("[Measures].[Gross Profit]");
 
-            // Adds the scatter chart to the dashboard and opens this
-            // dashboard in the Dashboard Viewer.
+            // Add the scatter chart to the dashboard and
+            // display the dashboard in the Dashboard Viewer.
             dashboard.Items.Add(scatterChart);
             dashboardViewer1.Dashboard = dashboard;
         }
--- Readme.md
+++ Readme.md
@@ -3,11 +3,9 @@
 
 * [Form1.cs](./CS/Dashboard_CreateScatterChart/Form1.cs) (VB: [Form1.vb](./VB/Dashboard_CreateScatterChart/Form1.vb))
 <!-- default file list end -->
-# How to bind a Scatter Chart dashboard item to data in code
+# How to Bind a Scatter Chart Dashboard Item to Data at Runtime
 
 
-The following example demonstrates how to bind a <a href="https://documentation.devexpress.com/#Dashboard/CustomDocument114805">Scatter Chart</a> dashboard item to data in code.
+This example demonstrates how to bind a [Scatter Chart](ttps://docs.devexpress.com/Dashboard/114805) dashboard item to data in code.
 
-<br/>
-
-
+![screenshot](/images/screenshot.png)
